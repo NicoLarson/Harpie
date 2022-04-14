86b814677cfc91153dccd9d0aef267cdb91a94ad
Nicolas Yang <nicolas.yang.1@gmail.com>
2022-04-14 16:05:18 -0300

    Ajout du formulaire

--- src/Controller/HarpieController.php
+++ src/Controller/HarpieController.php
@@ -2,14 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\FicheODE;
+use App\Entity\Demandeur;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HarpieController extends AbstractController
 {
     /**
-     * @Route("/", name="app_harpie")
+     * @Route("/", name="menu")
      */
     public function index(): Response
     {
@@ -23,8 +29,29 @@ class HarpieController extends AbstractController
      */
     public function creationMission(): Response
     {
+        $ficheODE = new FicheODE();
+        $form = $this->createFormBuilder($ficheODE)
+            ->add('numeroMission')
+            ->add('typeMission')
+            ->add('force')
+            ->add('dateDemande')
+            ->add('denominationOperation')
+            ->add('dateDebut')
+            ->add('dateFin')
+            ->add('cadreLegal')
+            ->add('observation')
+            ->add('effectifParMissions')
+            ->add('effectifPirogierParMissions')
+            ->add('communicationParMissions')
+            ->add('manoeuvreParMissions')
+            ->add('transportParMissions')
+            ->add('cibleParMissions')
+            ->add('valider', SubmitType::class, ['label' => 'Soumettre', 'attr' => ['class' => 'btn btn-validation']])
+            ->getForm();
+
         return $this->render('harpie/creation_mission.html.twig', [
             'controller_name' => 'HarpieController',
+            'formFicheODE' => $form->createView(),
         ]);
     }
 }
