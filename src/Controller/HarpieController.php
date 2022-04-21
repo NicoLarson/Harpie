9f7bfe4ec212a8b34df078bbac0f27e93f25c0c5
Nicolas Yang <nicolas.yang.1@gmail.com>
2022-04-21 15:14:18 -0300

    update formulaire

--- src/Controller/HarpieController.php
+++ src/Controller/HarpieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\CadreLegal;
+use App\Entity\Etat;
 use App\Entity\Cible;
 use App\Entity\Force;
 use App\Entity\Effectif;
@@ -10,20 +12,21 @@ use App\Entity\Demandeur;
 use App\Entity\Manoeuvre;
 use App\Entity\Transport;
 use App\Entity\TypeMission;
+use Doctrine\ORM\Mapping\Entity;
 use App\Entity\EffectifCategorie;
 use App\Entity\ManoeuvreParMission;
 use App\Entity\CommunicationParMission;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Mapping\Entity;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class HarpieController extends AbstractController
 {
@@ -52,56 +55,116 @@ class HarpieController extends AbstractController
      */
     public function creationMission(Request $request, EntityManagerInterface $manager): Response
     {
-        $typeMission = $_GET['type_mission'];
 
         $ficheODE = new FicheODE();
         $formFicheODE = $this->createFormBuilder($ficheODE)
-            ->add('typeMission', EntityType::class, [
-                'class' => TypeMission::class,
-                'choice_label' => 'libelle',
-                'label' => 'Type de mission',
-                'attr' => [
-                    'placeholder' => 'Type de mission',
-                ],
-            ])
+            // TODO: Ajouter les champs de la fiche ODE 
+            // - id demandeur
             ->add(
                 'demandeur',
                 EntityType::class,
                 [
                     'class' => Demandeur::class,
-                    'choice_label' => 'id',
+                    'choice_label' => 'numeroIdentifiantGendarme',
+                    'choice_value' => 'id',
                     'label' => 'Demandeur',
                     'placeholder' => 'Choisissez un demandeur',
                     'attr' => [
-                        'class' => 'form-control',
+                        'class' => 'form-group',
                     ],
                 ]
             )
+            // - type de mission
+            ->add(
+                'typeMission',
+                EntityType::class,
+                [
+                    'class' => TypeMission::class,
+                    'choice_label' => 'libelle',
+                    'choice_value' => 'id',
+                    'label' => 'Type de mission',
+                    'placeholder' => 'Choisissez un type de mission',
+                    'attr' => [
+                        'class' => 'form-group',
+                    ],
+                ]
+            )
+            // - force
             ->add('force', EntityType::class, [
                 'class' => Force::class,
-                'choice_label' => 'id',
+                'choice_label' => 'libelle',
+                'choice_value' => 'id',
                 'label' => 'Force',
                 'attr' => [
                     'placeholder' => 'Force',
                 ],
             ])
+            // - denomination operation
+            // TODO Générer automatiquement à partir des données du formulaire
+            ->add('denominationOperation', TextareaType::class, [
+                'label' => 'Denomination de l\'opération',
+                'attr' => [
+                    'placeholder' => 'Denomination de l\'opération',
+                    'readonly' => 'readonly',
+                ],
+            ])
+
+            // - date debut
+            ->add('dateDebut', DateType::class, [
+                'label' => 'Date de début',
+                'widget' => 'single_text',
+                'attr' => [
+                    'placeholder' => 'Date de début',
+                ],
+            ])
+            // - date fin
+            ->add('dateFin', DateType::class, [
+                'label' => 'Date de fin',
+                'widget' => 'single_text',
+                'attr' => [
+                    'placeholder' => 'Date de fin',
+                ],
+            ])
+            // - cibles // TODO : Formulaire pour ajouter plusieurs cibles
+            // - effectifs // TODO : Formulaire pour ajouter plusieurs effectifs
+            // - effectifs piroguier // TODO : Formulaire pour ajouter plusieurs effectifs
+            //     - total effectifs // TODO : TOTAL DES EFFECTIFS
+            // - transport // TODO : Formulaire pour ajouter plusieurs transports
+            //     - appui feu // TODO : Ajouter appuis feu ou non
+            ->add('transportParMissions')
+
+            // - manoeuvre // TODO : Formulaire pour ajouter plusieurs manoeuvres
+            //     - mission 
+            //     - execution
+
+            // - cadre légal
+            ->add('cadreLegal', EntityType::class, [
+                'class' => CadreLegal::class,
+                'choice_label' => 'libelle',
+                'choice_value' => 'id',
+                'label' => 'Cadre légal',
+            ])
+            // - observation
+            ->add('observation', TextareaType::class, [
+                'label' => 'Observation',
+                'attr' => [
+                    'placeholder' => 'Observation',
+                ],
+            ])
+            // - communication // TODO : Formulaire pour ajouter plusieurs communications
+            ->add('communicationParMissions')
+
+
             ->add('etat', EntityType::class, [
-                'class' => Cible::class,
-                'choice_label' => 'id',
+                'class' => Etat::class,
+                'choice_label' => 'libelle',
+                'choice_value' => 'id',
                 'label' => 'Etat',
                 'attr' => [
                     'placeholder' => 'Etat',
                 ],
             ])
-            ->add('dateDebut')
-            ->add('dateDebut')
-            ->add('dateFin')
-            ->add('cadreLegal')
-            ->add('observation')
-            ->add('communicationParMissions')
-            ->add('transportParMissions')
-            ->add('force')
-            ->add('dateDemande')
+            // - BTN valider
             ->add('soumettre', SubmitType::class, ['label' => 'Soumettre', 'attr' => ['class' => 'btn btn-validation']])
             ->getForm();
 
@@ -109,81 +172,54 @@ class HarpieController extends AbstractController
 
         if ($formFicheODE->isSubmitted() && $formFicheODE->isValid()) {
             $ficheODEDTO = $formFicheODE->getData();
-            $ficheODE->setDateDemande(new \DateTime());
-            $ficheODE->setDateDebut(new \DateTime());
-            $ficheODE->setDateFin(new \DateTime());
+            // TODO: Le numéro de mission sera généré automatiquement
+            // - id demandeur // TODO : Ajouter le numéro de demandeur qui est connecté
             $ficheODE->setDemandeur($ficheODEDTO->getDemandeur());
+            // - type de mission
+            $ficheODE->setTypeMission($ficheODEDTO->getTypeMission());
+            // - force
             $ficheODE->setForce($ficheODEDTO->getForce());
+            // - n de mission
+            $ficheODE->setNumeroMission("123123123");
+            // - date de demande
+            $ficheODE->setDateDemande(new \DateTime());
+            // - denomination operation 
+            // TODO Générer automatiquement à partir des données du formulaire
+            $ficheODE->setDenominationOperation('test');
+            // - date debut
+            $ficheODE->setDateDebut($ficheODEDTO->getDateDebut());
+            // - date fin
+            $ficheODE->setDateFin($ficheODEDTO->getDateFin());
+            // - cibles
+
+            // - effectifs
+            // - effectifs piroguier
+            //     - total effectifs
+            // - transport
+            //     - appui feu
+            // - manoeuvre
+            //     - mission
+            //     - execution
+            // - cadre légal
+            $ficheODE->setCadreLegal($ficheODEDTO->getCadreLegal());
+            // - observation
+            $ficheODE->setObservation($ficheODEDTO->getObservation());
+            // - communication
+            // - BTN valider            
             $ficheODE->setEtat($ficheODEDTO->getEtat());
             $manager->persist($ficheODE);
             $manager->flush();
+            if ($ficheODEDTO->getDateDebut() > $ficheODEDTO->getDateFin()) {
+                $this->addFlash('danger', 'La date de début doit être inférieure à la date de fin');
+            } else {
 
-            $this->addFlash('success', 'Votre article a été ajouté.');
-            return $this->redirectToRoute('creation_mission');
+                return $this->redirectToRoute('menu');
+            }
         }
 
         return $this->render('harpie/creation_mission.html.twig', [
             'controller_name' => 'HarpieController',
-            'typeMission' => $typeMission,
-
             'formFicheODE' => $formFicheODE->createView(),
-            // 'formCible' => $formCible->createView(),
-            // 'formForce' => $formForce->createView(),
-            // 'formEffectif' => $formEffectif->createView(),
-            // 'formEffectifPiroguier' => $formEffectifPiroguier->createView(),
-            // 'formTransport' => $formTransport->createView(),
-            // 'formManoeuvreParMission' => $formManoeuvreParMission->createView(),
-            // 'formCommunicationParMission' => $formCommunicationParMission->createView(),
         ]);
     }
 }
-
-
-   // $cible = new Cible();
-        // $formCible = $this->createFormBuilder($cible)
-        //     ->add('latitudeN')
-        //     ->add('longitudeO')
-        //     ->add('libelle')
-        //     ->add('Valider', SubmitType::class, ['label' => 'Valider', 'attr' => ['class' => 'btn btn-validation-popup']])
-        //     ->getForm();
-
-        // $force = new Force();
-        // $formForce = $this->createFormBuilder($force)
-        //     ->add('libelle')
-        //     ->getForm();
-
-        // $effectif = new Effectif();
-        // $formEffectif = $this->createFormBuilder($effectif)
-        //     ->add('effectifCategorie')
-        //     ->add('effectifSousCategorie')
-        //     ->add('volume')
-        //     ->add('Valider', SubmitType::class, ['label' => 'Valider', 'attr' => ['class' => 'btn btn-validation-popup']])
-        //     ->getForm();
-
-        // $effectifPiroguier = new Effectif();
-        // $formEffectifPiroguier = $this->createFormBuilder($effectifPiroguier)
-        //     ->add('effectifCategorie')
-        //     ->add('effectifSousCategorie')
-        //     ->add('volume')
-        //     ->add('Valider', SubmitType::class, ['label' => 'Valider', 'attr' => ['class' => 'btn btn-validation-popup']])
-        //     ->getForm();
-
-        // $transport = new Transport();
-        // $formTransport = $this->createFormBuilder($transport)
-        //     ->add('categorie')
-        //     ->add('sousCategorie')
-        //     ->add('appuiFeu')
-        //     ->add('Valider', SubmitType::class, ['label' => 'Valider', 'attr' => ['class' => 'btn btn-validation-popup']])
-        //     ->getForm();
-
-        // $manoeuvreParMission = new ManoeuvreParMission();
-        // $formManoeuvreParMission = $this->createFormBuilder($manoeuvreParMission)
-        //     ->add('manoeuvre')
-        //     ->add('execution')
-        //     ->add('Valider', SubmitType::class, ['label' => 'Valider', 'attr' => ['class' => 'btn btn-validation-popup']])
-        //     ->getForm();
-
-        // $communicationParMission = new CommunicationParMission();
-        // $formCommunicationParMission = $this->createFormBuilder($communicationParMission)
-        //     ->add('communication')
-        //     ->getForm();
